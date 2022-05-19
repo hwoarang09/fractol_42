9a46df85bcb021fdb33fcf71fa11a9777b6b4f2c
hwoarang09 <hwoarang09@naver.com>
2022-05-19 21:44:21 +0900

    hi

---
+++ mandelbrot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class MandelbrotSet:
+    max_iterations: int
+
+    def __contains__(self, c: complex) -> bool:
+        z = 0
+        for _ in range(self.max_iterations):
+            z = z**2 + c
+            if abs(z) > 2:
+                return False
+        return True
+
